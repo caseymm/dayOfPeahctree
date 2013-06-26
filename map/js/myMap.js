6f86721f7b69c6df4949f2b7efb99e5e078f7d52
caseymm <caseymm@gmail.cpom>
2013-06-26 19:31:08 -0400

    boxes work

--- map/js/myMap.js
+++ map/js/myMap.js
@@ -1,41 +1,79 @@
-var Marker = L.AwesomeMarkers.icon({
-  icon: 'food', 
-  color: 'red',
-  labelAnchor: [7, -33]
-})
-
-var Cook = L.marker([33.847305,-84.366031], {icon: Marker}).bindLabel('Cook Hall Atlanta').bindPopup('<b>Cook Hall Atlanta</b> <br /> 3377 Peachtree Road NE, <br /> Atlanta GA 30325'),
-    RThomas = L.marker([33.804274,-84.39369], {icon: Marker}).bindLabel('R. Thomas').bindPopup('<b>R. Thomas</b> <br /> 1812 Peachtree Street, NW, <br /> Atlanta, Georgia 30309'),
-    TAP = L.marker([33.786691,-84.383776], {icon: Marker}).bindLabel('TAP').bindPopup('<b>TAP</b> <br /> 1180 Peachtree Street, NE, <br /> Atlanta, GA 30303'),
-    Flying = L.marker([33.781983,-84.380107], {icon: Marker}).bindLabel('Flying Biscuit').bindPopup('<b>Flying Biscuit</b> <br /> 1001 Piedmont Ave NE <br /> Atlanta, GA 30309'),
-    Park = L.marker([33.782179,-84.369078], {icon: Marker}).bindLabel('Park Tavern').bindPopup('<b>Palmetto Branch</b> <br /> 500 10th Street N.E., <br /> Atlanta, GA 30309'),
-    
-    
-//var cmAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-   // cmUrl = 'http://{s}.tile.cloudmade.com/825c6664ffdf4e619e51fb7dd3b47005/98490/256/{z}/{x}/{y}.png';
-
-//var regular   = L.tileLayer(cmUrl, {styleId: 997, attribution: cmAttr});
-
-		map = L.map('map', {
+var map = L.map('map', {
 			center: new L.LatLng(33.821656,-84.373627),
 			zoom: 12,
-			layers: [Cook, RThomas,TAP, Flying, Park],
 		});
-
 
 L.tileLayer('http://{s}.tile.cloudmade.com/825c6664ffdf4e619e51fb7dd3b47005/98490/256/{z}/{x}/{y}.png', {
                         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
                         maxZoom: 18
                         }).addTo(map);
 
-//var baseLayers = {};
+var Marker = L.AwesomeMarkers.icon({
+  icon: 'food', 
+  color: 'red',
+  labelAnchor: [7, -33]
+})
+
+var start = L.icon({
+    iconUrl: '../dayOfPeachtree/map/images/flag_green3.png',
+
+    iconSize:     [32, 32], // size of the icon
+    iconAnchor:   [4, 30], // point of the icon which will correspond to marker's location
+});
+
+var finish = L.icon({
+    iconUrl: '../dayOfPeachtree/map/images/flag_red3.png',
+
+    iconSize:     [32, 32], // size of the icon
+    iconAnchor:   [4, 30], // point of the icon which will correspond to marker's location
+}); 
+
+L.marker([33.847305,-84.366031], {icon: Marker}).addTo(map)
+  .bindLabel('Cook Hall').bindPopup('<a href="http:/cookhallatlanta.com"><b>Cook Hall</b></a> <br /> 3377 Peachtree Road NE, <br /> Atlanta GA 30325');
+L.marker([33.804274,-84.39369], {icon: Marker}).addTo(map)
+  .bindLabel('R. Thomas Deluxe Grill').bindPopup('<a href="http://www.rthomasdeluxegrill.net"><b>R. Thomas Deluxe Grill</b></a> <br /> 1812 Peachtree Street, NW, <br /> Atlanta, Georgia 30309');
+L.marker([33.786691,-84.383776], {icon: Marker}).addTo(map)
+  .bindLabel('TAP').bindPopup('<a href="http://www.tapat1180.com"><b>TAP</b></a> <br /> 1180 Peachtree Street, NE, <br /> Atlanta, GA 30303');
+L.marker([33.781983,-84.380107], {icon: Marker}).addTo(map)
+  .bindLabel('The Flying Biscuit Cafe').bindPopup('<a href="http://www.flyingbiscuit.com"><b>The Flying Biscuit Cafe</b></a> <br /> 1001 Piedmont Ave NE <br /> Atlanta, GA 30309');
+//L.marker([33.782179,-84.369078], {icon: Marker}).addTo(map)
+  //.bindLabel('Park Tavern').bindPopup('<b>Park Tavern</b> <br /> 500 10th Street N.E., <br /> Atlanta, GA 30309');
+  
+L.marker([33.85356,-84.359508], {icon: start}).addTo(map).bindLabel('Start');
+L.marker([33.781751,-84.372811], {icon: finish}).addTo(map).bindLabel('Finish');
 
 
-//L.control.layers(baseLayers).addTo(map);
-    
-//var marker = L.marker([33.751748,-84.391479]).bindLabel('Look revealing label!').addTo(map);
-    
-//var popup = L.popup()
-    //.setLatLng([33.751748,-84.491479])
-    //.setContent("I am a standalone popup.")
-    //.openOn(map);
+ 
+ var path = L.polyline([
+				[33.85356,-84.359508],
+				[33.849568,-84.362941],
+				[33.848891,-84.364743],
+				[33.848998,-84.365687],
+				[33.846859,-84.368091],
+				[33.845327,-84.368691],
+				[33.840372,-84.377317],
+				[33.840301,-84.378519],
+				[33.839017,-84.380236],
+				[33.834134,-84.382854],
+				[33.8295,-84.387445],
+				[33.822797,-84.387445],
+				[33.817913,-84.388947],
+				[33.81588,-84.390879],
+				[33.815167,-84.391608],
+				[33.812992,-84.392509],
+				[33.811851,-84.392209],
+				[33.806039,-84.393926],
+				[33.802544,-84.39281],
+				[33.800547,-84.392638],
+				[33.799406,-84.392166],
+				[33.79855,-84.391265],
+				[33.79723,-84.389119],
+				[33.796696,-84.387875],
+				[33.794734,-84.387724],
+				[33.789081,-84.384077],
+				[33.786584,-84.383154],
+				[33.784462,-84.382961],
+				[33.783196,-84.383862],
+				[33.781751,-84.384034],
+				[33.781751,-84.372811]], {}).addTo(map);
+ 
